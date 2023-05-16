5d88d6b0f908329e21aed9d431efaab354adc92d
4ard <112971420+4ard@users.noreply.github.com>
2023-05-16 18:15:49 +0300

    index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ foreach($superpowers as $checking){
 
 $user = 'u52971';
 $pass = '6342896';
-$db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$db = new PDO('mysql:host=localhost;dbname=u52971', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 try {
   $stmt = $db->prepare("INSERT INTO form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio");
   $stmt->bindParam(':name', $name);
